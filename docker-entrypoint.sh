f1160aec50e1526a30391c38eb859dc5567dfa46
Damjan Kovač <4171751+KovacDamjan@users.noreply.github.com>
2026-08-31 10:02:48 +0200

    Make entrypoint resilient to empty Container Manager command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,8 @@ set -eu
 mkdir -p /app/data
 chown bridge:bridge /app/data
 
-exec gosu bridge "$@"
+if [ "$#" -eq 0 ]; then
+  set -- uvicorn app.main:app --host 0.0.0.0 --port 8095
+fi
 
+exec gosu bridge "$@"
