75fdc1740d0794ca0e050f158610cb1b373a97d1
Damjan Kovač <4171751+KovacDamjan@users.noreply.github.com>
2026-08-31 10:10:33 +0200

    Fix ownership of persistent SQLite data

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,8 @@
 set -eu
 
 mkdir -p /app/data
-chown bridge:bridge /app/data
+chown -R bridge:bridge /app/data
+chmod 0775 /app/data
 
 if [ "$#" -eq 0 ]; then
   set -- uvicorn app.main:app --host 0.0.0.0 --port 8095
